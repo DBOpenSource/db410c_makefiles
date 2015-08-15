e0c70cc7e4979f7d7613e079a69ace968addaf5a
Mark Charlebois <charlebm@gmail.com>
2015-08-15 00:37:01 -0700

    Check for missing command
    Signed-off-by: Mark Charlebois <charlebm@gmail.com>

--- db410c.mk
+++ db410c.mk
@@ -56,9 +56,13 @@ ifeq ($(IMAGE),)
 $(error IMAGE Undefined)
 endif
 
+ifeq ($(shell which fdtget),)
+$(error Missing fdtget, run: sudo apt-get install device-tree-compiler)
+endif
+
 INITRD:=$(DOWNLOAD_DIR)/initrd.img-4.0.0-linaro-lt-qcom
 DB410C_KERNEL?=db410c-linux
-KERNEL_VERSION?=origin/release/qcomlt-4.0
+KERNEL_VERSION?=release/qcomlt-4.0
 KERNEL_BRANCH:=_build_branch
 SKALES?=skales
 
@@ -66,11 +70,9 @@ FIRMWARE_ZIP:=$(FIRMWARE_DEST_DIR)/linux-ubuntu-board-support-package-v1.zip
 
 ifneq ($(FIRMWARE_UNPACK_DIR),)
 
-$(FIRMWARE_UNPACK_DIR)/.unpacked: $(FIRMWARE_ZIP)
-	mkdir -p $(FIRMWARE_UNPACK_DIR)
+_firmware_unpack: $(FIRMWARE_ZIP) $(DOWNLOAD_DIR)
 	[ -f $(DOWNLOAD_DIR)/proprietary-ubuntu-1.tgz ] || (cd $(DOWNLOAD_DIR) && unzip $(FIRMWARE_ZIP))
-	[ -f $(FIRMWARE_UNPACK_DIR)/.unpacked ] || tar -C $(FIRMWARE_UNPACK_DIR) -xzpf $(DOWNLOAD_DIR)/proprietary-ubuntu-1.tgz --strip 1
-	touch $@
+	[ -f $(FIRMWARE_UNPACK_DIR)/.unpacked ] || (cd $(FIRMWARE_UNPACK_DIR) && tar xzpf $(DOWNLOAD_DIR)/proprietary-ubuntu-1.tgz --strip 1)
 
 endif
 
